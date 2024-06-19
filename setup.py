cda35b36864b4b334e580f2b1ffd5cb5c4761a0b
danielbloy <daniel_bloy@hotmail.com>
2024-06-19 21:26:07 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pico-interactive',
+    version='0.1',
+    description='A module to help build interactive displays',
+    author='Daniel Bloy',
+    author_email='daniel.bloy@danielbloy.com',
+    packages=['framework'],
+)
