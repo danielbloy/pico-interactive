97a5630ff42b449b2ce86126233de6092b84074b
danielbloy <daniel_bloy@hotmail.com>
2024-06-24 18:02:10 +0100

    Add outline BuzzerController test class.

---
+++ tests/framework/test_buzzer.py
@@ -0,0 +1,33 @@
+import pytest
+
+from framework.buzzer import BuzzerController
+from framework.polyfills.buzzer import Buzzer
+
+
+class TestBuzzer(Buzzer):
+    def __init__(self):
+        super().__init__(None)
+
+        self.update_count = 0
+
+    def update(self) -> None:
+        self.update_count += 1
+
+
+class TestBuzzerController:
+
+    def test_creating_with_none_buzzer_errors(self) -> None:
+        """
+        Validates that a ButzzerController cannot be constructed with
+        a None value.
+        """
+        with pytest.raises(ValueError):
+            BuzzerController(None)
+
+    def test_creating_with_string_errors(self) -> None:
+        """
+        Validates that a BuzzerController cannot be constructed with
+        a value that is not a Buzzer.
+        """
+        with pytest.raises(ValueError):
+            BuzzerController("")
