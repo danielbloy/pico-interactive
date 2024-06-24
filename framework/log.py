68f1e43ab5cc0f884b6fad532efd88563f6ca423
danielbloy <daniel_bloy@hotmail.com>
2024-06-24 07:56:36 +0100

    Add adafruit logging package.

--- framework/log.py
+++ framework/log.py
@@ -1,33 +1,75 @@
-# Python logging:
-# https://realpython.com/python-logging/
-# https://docs.python.org/3/library/logging.html
-#
-# CircuitPython logging:
-# https://docs.circuitpython.org/projects/logging/en/latest/api.html
+# This is a very thin layer over the top of Python and CircuitPython logging.
+# It offers a very basic set of functions using the single fallback logger.
+# This is for simplicity as we are expecting this to run on a microcontroller
+# where complex logging is simply not available.
+from framework.environment import is_running_on_desktop
+
+if is_running_on_desktop():
+    import logging
+
+else:
+    import adafruit_logging as logging
+
+from logging import CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+
+# Prevents PyCharm from clearing up the unused imports.
+__CRITICAL = CRITICAL
+__ERROR = ERROR
+__WARNING = WARNING
+__INFO = INFO
+__DEBUG = DEBUG
+__NOTSET = NOTSET
+
+logger = logging.getLogger(__name__)
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(format=FORMAT, level=WARNING)
 
 
-def stacktrace(exception: Exception) -> None:
+def set_log_level(level) -> None:
+    """
+    Sets the logging level to use in the same way as Logging. Defaults to WARNING.
+
+    :param level: A number, usually oOne of CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+    """
+    logging.basicConfig(level=level)
+
+
+def stacktrace(e: Exception) -> None:
+    """
+    Puts a stacktrace out for the exception using the DEBUG log level.
+
+    :param e: The exception whose stack trace we want to log.
+    """
     import traceback
-    for s in traceback.format_exception(exception):
-        debug(s)
+    for s in traceback.format_exception(e):
+        logger.debug(s)
 
 
-# TODO: this should be re-routed to a suitable logging framework based on the execution environment.
 def debug(message: str) -> None:
-    print(message)
+    """Writes message at the DEBUG log level."""
+    logger.debug(message)
 
 
 def info(message: str) -> None:
-    print(message)
+    """Writes message at the INFO log level."""
+    logger.info(message)
 
 
 def warn(message: str) -> None:
-    print(message)
+    """Writes message at the WARNING log level."""
+    logger.warning(message)
 
 
 def error(message: str) -> None:
-    print(message)
+    """Writes message at the ERROR log level."""
+    logger.error(message)
+
+
+def exception(message: str) -> None:
+    """Writes message at the ERROR log level with a stacktrace."""
+    logger.exception(message)
 
 
 def critical(message: str) -> None:
-    print(message)
+    """Writes message at the CRITICAL log level."""
+    logger.critical(message)
--- tests/on_device/1_validate_runner.py
+++ tests/on_device/1_validate_runner.py
@@ -1,34 +1,37 @@
 import asyncio
 
+from framework.log import set_log_level, debug, info, INFO
 from framework.runner import Runner
 
 if __name__ == '__main__':
     i: int = 0
 
+    set_log_level(INFO)
+
 
     async def callback() -> None:
         global i
         i += 1
-        print(f'Callback: i={i}')
+        debug(f'Callback: i={i}')
         runner.cancel = i == 30
 
 
     async def runs_forever_task():
         while True:
-            print("LOOP: runs_forever_task")
+            info("LOOP: runs_forever_task")
             await asyncio.sleep(2.0)
 
 
     async def completes_task():
-        print("START: completes_task")
+        info("START: completes_task")
         await asyncio.sleep(0.005)
-        print("FINISH: completes_task")
+        info("FINISH: completes_task")
 
 
     async def raises_exception_task():
-        print("START: raises_exception_task")
+        info("START: raises_exception_task")
         await asyncio.sleep(0.005)
-        print("EXCEPTION: raises_exception_task")
+        info("EXCEPTION: raises_exception_task")
         raise Exception("Raised by task 3")
 
 
