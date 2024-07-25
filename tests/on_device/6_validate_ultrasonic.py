63c27774b5f0c2855f03b2887cd62dbcab180e5d
danielbloy <daniel_bloy@hotmail.com>
2024-07-25 18:35:35 +0100

    Added the Ultrasonic test file

---
+++ tests/on_device/6_validate_ultrasonic.py
@@ -0,0 +1,47 @@
+import time
+
+from interactive.button import ButtonController
+from interactive.environment import are_pins_available
+from interactive.log import set_log_level, INFO, info
+from interactive.polyfills.button import new_button
+from interactive.polyfills.ultrasonic import new_ultrasonic
+from interactive.runner import Runner
+
+BUTTON_PIN = None
+ULTRASONIC_TRIGGER_PIN = None
+ULTRASONIC_ECHO_PIN = None
+
+if are_pins_available():
+    # noinspection PyPackageRequirements
+    import board
+
+    BUTTON_PIN = board.GP27
+    ULTRASONIC_TRIGGER_PIN = board.GP27
+    ULTRASONIC_ECHO_PIN = board.GP27
+
+if __name__ == '__main__':
+    set_log_level(INFO)
+
+    ultrasonic = new_ultrasonic(ULTRASONIC_TRIGGER_PIN, ULTRASONIC_ECHO_PIN)
+
+
+    async def single_click_handler() -> None:
+        info(f"Distance: {ultrasonic.distance}")
+
+
+    runner = Runner()
+
+    button = new_button(BUTTON_PIN)
+    button_controller = ButtonController(button)
+    button_controller.add_single_click_handler(single_click_handler)
+    button_controller.register(runner)
+
+    # Allow the application to only run for a defined number of seconds.
+    finish = time.monotonic() + 10
+
+
+    async def callback() -> None:
+        runner.cancel = time.monotonic() > finish
+
+
+    runner.run(callback)
