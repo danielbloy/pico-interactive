0174a257f81d59f3f4c3d24081ff684b1448f998
danielbloy <daniel_bloy@hotmail.com>
2024-07-30 21:51:48 +0100

    Improved the display handling code.

--- halloween/2024/path/code.py
+++ halloween/2024/path/code.py
@@ -1,5 +1,6 @@
 # Entry point for a path node. This supports both path nodes that are on either
 # side of the path. One will be the primary node and the other the secondary.
+import asyncio
 import time
 
 from interactive.animation import Flicker
@@ -21,9 +22,9 @@ ULTRASONIC_ECHO_PIN = None
 
 TRIGGER_DISTANCE = 100
 TRIGGER_DURATION = 60
-DISPLAY_DURATION = 30
 
 SKULL_BRIGHTNESS = 1.0
+SKULL_OFF = 0.0
 SKULL_SPEED = 0.1
 SKULL_COLOUR = ORANGE
 SKULL_PINS = [None, None, None, None, None, None]
@@ -42,35 +43,42 @@ if are_pins_available():
 
     SKULL_PINS = [board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15]
 
-triggered = True
-running = False
-stop_time = 0
 pixels = [new_pixels(pin, 8, brightness=SKULL_BRIGHTNESS) for pin in SKULL_PINS if pin is not None]
 animations = [Flicker(pixel, speed=SKULL_SPEED, color=SKULL_COLOUR) for pixel in pixels]
 
 
-# TODO: The run_display() and setup via the trigger could be moved into Interactive with
-#       hooks for starting, stopping and running.
+async def stop_display() -> None:
+    for pixel in pixels:
+        pixel.brightness = SKULL_OFF
+        pixel.show()
+
+
+async def start_display() -> None:
+    for pixel in pixels:
+        pixel.brightness = SKULL_BRIGHTNESS
+
+    t1 = asyncio.create_task(test_task_1())
+    t2 = asyncio.create_task(test_task_2())
+
 
 async def run_display() -> None:
-    global triggered, running, stop_time
+    for animation in animations:
+        animation.animate()
 
-    # Check for stop display
-    if running and time.time() >= stop_time:
-        # TODO: Stop the pixels
-        running = False
 
-    # Check for start display
-    if triggered and not running:
-        # Start display
-        stop_time = time.time() + DISPLAY_DURATION
-        running = True
+async def test_task_1() -> None:
+    info("Start test task 1")
+    await asyncio.sleep(2)
+    for pixel in pixels:
+        pixel.brightness = SKULL_OFF
+        pixel.show()
+    info("End test task 1")
 
-    triggered = False
 
-    if running:
-        for animation in animations:
-            animation.animate()
+async def test_task_2() -> None:
+    info("Start test task 2")
+    await asyncio.sleep(1)
+    info("End test task 2")
 
 
 if __name__ == '__main__':
@@ -91,6 +99,12 @@ if __name__ == '__main__':
     config.buzzer_pin = BUZZER_PIN
     config.button_pin = BUTTON_PIN
     config.buzzer_volume = BUZZER_VOLUME
+    config.ultrasonic_trigger_pin = ULTRASONIC_TRIGGER_PIN
+    config.ultrasonic_echo_pin = ULTRASONIC_ECHO_PIN
+
+    triggered = False
+    running = False
+    stop_time = 0
 
 
     async def trigger_handler(distance: float, actual: float) -> None:
@@ -99,9 +113,31 @@ if __name__ == '__main__':
         triggered = True
 
 
+    async def trigger_events() -> None:
+        global triggered, running, stop_time
+
+        # Check for stop display
+        if running and time.time() >= stop_time:
+            info("Stop running trigger event")
+            running = False
+            await stop_display()
+
+        # Check for start display
+        if triggered and not running:
+            info("Start running trigger event")
+            stop_time = time.time() + TRIGGER_DURATION
+            running = True
+            await start_display()
+
+        triggered = False
+
+        if running:
+            await run_display()
+
+
     interactive = Interactive(config)
     interactive.trigger.add_trigger(TRIGGER_DISTANCE, trigger_handler, TRIGGER_DURATION)
-    interactive.runner.add_loop_task(run_display)
+    interactive.runner.add_loop_task(trigger_events)
 
 
     async def callback() -> None:
