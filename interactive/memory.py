9aaa76e9df53bb066730be68c147fb579ea47409
danielbloy <daniel_bloy@hotmail.com>
2024-08-05 07:41:45 +0100

    Added memory helper class.

---
+++ interactive/memory.py
@@ -0,0 +1,9 @@
+# This module contains utility functions for examining how much RAM the
+# system has availale whilst it is running.
+#
+# Resources:
+# * https://learn.adafruit.com/Memory-saving-tips-for-CircuitPython?view=all
+# * https://docs.circuitpython.org/en/latest/shared-bindings/memorymonitor/index.html
+# * https://github.com/kmatch98/CircuitPython_memory_saving
+# * https://electronics.stackexchange.com/questions/476410/how-to-free-up-memory-in-a-circuitpython-board
+# * https://www.adafruitdaily.com/2017/03/31/measure-your-memory-usage-with-mem_info/
