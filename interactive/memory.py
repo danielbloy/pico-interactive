e6160d322dc65a3c25d9b38c581f63a7af7fb5ee
Daniel Bloy <daniel.bloy@featurespace.co.uk>
2024-08-05 10:38:35 +0100

    Tiny update

--- interactive/memory.py
+++ interactive/memory.py
@@ -3,7 +3,30 @@
 #
 # Resources:
 # * https://learn.adafruit.com/Memory-saving-tips-for-CircuitPython?view=all
-# * https://docs.circuitpython.org/en/latest/shared-bindings/memorymonitor/index.html
-# * https://github.com/kmatch98/CircuitPython_memory_saving
 # * https://electronics.stackexchange.com/questions/476410/how-to-free-up-memory-in-a-circuitpython-board
 # * https://www.adafruitdaily.com/2017/03/31/measure-your-memory-usage-with-mem_info/
+
+# TODO: To determine how much free RAM there is in the system run (this will not count phantom memory as free, run the garbage collector):
+import gc
+gc.mem_free()
+
+
+
+# TODO: Before Interactive runs, we should call the garbage collection and memory manager.
+import gc
+gc.collect()
+
+# TODO: We should also run the gc.collect() at various points during start up.
+# TODO: We need to investigate what is using memory and how expensive:
+    * Framework: Interactive
+    * Button
+    * Buzzer
+    * NeoPixels
+    * Ultrasonic
+    * Audio
+    * Network
+    
+# Memory optimisation:  
+# * TODO: Delete large variables using: del <large_variable>
+# * TODO: Examine where lists and dictionaries are used as they can grow slowly
+# * TODO: Examine all uses of strings
