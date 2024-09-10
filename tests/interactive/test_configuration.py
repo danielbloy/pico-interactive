a1f5c83b998ecf91d7d405c792a12c5ccd13b4ff
Daniel Bloy <daniel.bloy@featurespace.co.uk>
2024-09-10 09:25:55 +0100

    Added some tests.

--- tests/interactive/test_configuration.py
+++ tests/interactive/test_configuration.py
@@ -18,82 +18,55 @@ class TestNode:
         assert configuration.TEST_VALUE == 123.456
         assert configuration.TEST_STRING == "Hello world!"
 
-    def test_config_returns_only_desired_config(self) -> None:
+    def test_config_returns_only_desired_config(self, monkeypatch) -> None:
         """
         Validates the boolean options that control which configuration properties
         are loaded, works as expected. This will mess with state so will restore
         all values it finds.
         """
-        # Copy original configuration state.
-        original_config = configuration.get_node_config()
+        monkeypatch.setattr(configuration, 'BUTTON_PIN', "button_pin")
+        monkeypatch.setattr(configuration, 'BUZZER_PIN', "buzzer_pin")
+        monkeypatch.setattr(configuration, 'BUZZER_VOLUME', "buzzer_volume")
+        monkeypatch.setattr(configuration, 'AUDIO_PIN', "audio_pin")
+        monkeypatch.setattr(configuration, 'ULTRASONIC_TRIGGER_PIN', "ultrasonic_trigger_pin")
+        monkeypatch.setattr(configuration, 'ULTRASONIC_ECHO_PIN', "ultrasonic_echo_pin")
+        monkeypatch.setattr(configuration, 'TRIGGER_DISTANCE', "trigger_distance")
+        monkeypatch.setattr(configuration, 'TRIGGER_DURATION', "trigger_duration")
 
-        configuration.BUTTON_PIN = "button_pin"
-        configuration.BUZZER_PIN = "buzzer_pin"
-        configuration.BUZZER_VOLUME = "buzzer_volume"
-        configuration.AUDIO_PIN = "audio_pin"
-        configuration.ULTRASONIC_TRIGGER_PIN = "ultrasonic_trigger_pin"
-        configuration.ULTRASONIC_ECHO_PIN = "ultrasonic_echo_pin"
-        configuration.TRIGGER_DISTANCE = "trigger_distance"
-        configuration.TRIGGER_DURATION = "trigger_duration"
+        defaults = Config()
 
-        try:
-            defaults = Config()
+        assert not defaults.network
+        config = configuration.get_node_config(network=True)
+        assert config.network
 
-            assert not defaults.network
-            config = configuration.get_node_config(network=True)
-            assert config.network
+        config = configuration.get_node_config(button=False)
+        assert config.button_pin == defaults.button_pin
 
-            config = configuration.get_node_config(button=False)
-            assert config.button_pin == defaults.button_pin
+        config = configuration.get_node_config(buzzer=False)
+        assert config.buzzer_pin == defaults.buzzer_pin
+        assert config.buzzer_volume == defaults.buzzer_volume
 
-            config = configuration.get_node_config(buzzer=False)
-            assert config.buzzer_pin == defaults.buzzer_pin
-            assert config.buzzer_volume == defaults.buzzer_volume
+        config = configuration.get_node_config(audio=False)
+        assert config.audio_pin == defaults.audio_pin
 
-            config = configuration.get_node_config(audio=False)
-            assert config.audio_pin == defaults.audio_pin
+        config = configuration.get_node_config(ultrasonic=False)
+        assert config.ultrasonic_trigger_pin == defaults.ultrasonic_trigger_pin
+        assert config.ultrasonic_echo_pin == defaults.ultrasonic_echo_pin
+        assert config.trigger_distance == defaults.trigger_distance
+        assert config.trigger_duration == defaults.trigger_duration
 
-            config = configuration.get_node_config(ultrasonic=False)
-            assert config.ultrasonic_trigger_pin == defaults.ultrasonic_trigger_pin
-            assert config.ultrasonic_echo_pin == defaults.ultrasonic_echo_pin
-            assert config.trigger_distance == defaults.trigger_distance
-            assert config.trigger_duration == defaults.trigger_duration
-
-        finally:
-            # Restore original configuration state.
-            configuration.BUTTON_PIN = original_config.button_pin
-            configuration.BUZZER_PIN = original_config.buzzer_pin
-            configuration.BUZZER_VOLUME = original_config.buzzer_volume
-            configuration.AUDIO_PIN = original_config.audio_pin
-            configuration.ULTRASONIC_TRIGGER_PIN = original_config.ultrasonic_trigger_pin
-            configuration.ULTRASONIC_ECHO_PIN = original_config.ultrasonic_echo_pin
-            configuration.TRIGGER_DISTANCE = original_config.trigger_distance
-            configuration.TRIGGER_DURATION = original_config.trigger_duration
-
-    def test_details_returns_correct_values(self) -> None:
+    def test_details_returns_correct_values(self, monkeypatch) -> None:
         """
         Validates details returns the correct values. This will mess with state
         so will restore all values it finds.
         """
-        # Copy original configuration state.
-        original_name = configuration.NODE_NAME
-        original_role = configuration.NODE_ROLE
-        original_coordinator = configuration.NODE_COORDINATOR
+        monkeypatch.setattr(configuration, 'NODE_NAME', "JacKsoN")
+        monkeypatch.setattr(configuration, 'NODE_ROLE', "RaNdOm")
+        monkeypatch.setattr(configuration, 'NODE_COORDINATOR', "arbiter")
 
-        configuration.NODE_NAME = "JacKsoN"
-        configuration.NODE_ROLE = "RaNdOm"
-        configuration.NODE_COORDINATOR = "arbiter"
-
-        try:
-            details = configuration.details()
-            assert len(details.keys()) == 3
-            assert set(details.keys()) == {FIELD_NAME, FIELD_ROLE, FIELD_COORDINATOR}
-            assert details[FIELD_NAME] == "JacKsoN"
-            assert details[FIELD_ROLE] == "RaNdOm"
-            assert details[FIELD_COORDINATOR] == "arbiter"
-
-        finally:
-            # Restore original configuration state.
-            configuration.NODE_NAME = original_name
-            configuration.NODE_ROLE = original_role
-            configuration.NODE_COORDINATOR = original_coordinator
+        details = configuration.details()
+        assert len(details.keys()) == 3
+        assert set(details.keys()) == {FIELD_NAME, FIELD_ROLE, FIELD_COORDINATOR}
+        assert details[FIELD_NAME] == "JacKsoN"
+        assert details[FIELD_ROLE] == "RaNdOm"
+        assert details[FIELD_COORDINATOR] == "arbiter"
