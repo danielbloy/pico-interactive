93ace182c6b540a07b5df0e918364814409520d2
danielbloy <daniel_bloy@hotmail.com>
2024-09-29 21:04:33 +0100

    Added cauldron code

---
+++ halloween/2024/path/network_config.py
@@ -0,0 +1,15 @@
+# NOTE: Rename this to config.py on the primary node network microcontroller.
+# This code runs on a standard Pico 2040
+import board
+
+BUTTON_PIN = board.GP26
+
+LOCAL_TRIGGER_PIN = board.GP21
+REMOTE_TRIGGER_PIN = board.GP0
+TRIGGER_DURATION = 30
+REPORT_RAM = True
+REPORT_RAM_PERIOD = 10
+
+from interactive.log import CRITICAL
+
+LOG_LEVEL = CRITICAL
