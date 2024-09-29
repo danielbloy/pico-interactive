46da69f81f255cd0f372dac07384570d1d83cb09
danielbloy <daniel_bloy@hotmail.com>
2024-09-29 21:31:54 +0100

    Add in witch node

---
+++ halloween/2024/witch/witch_network_config.py
@@ -0,0 +1,15 @@
+# NOTE: Rename this to config.py on the primary node network microcontroller.
+# This code runs on a standard Pico 2040
+import board
+
+BUTTON_PIN = board.GP0
+
+LOCAL_TRIGGER_PIN = board.GP9
+REMOTE_TRIGGER_PIN = board.GP1
+TRIGGER_DURATION = 10
+REPORT_RAM = True
+REPORT_RAM_PERIOD = 10
+
+from interactive.log import CRITICAL
+
+LOG_LEVEL = CRITICAL
