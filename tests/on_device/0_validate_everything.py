9ef4eadec05df9587768779930d9fe966b031273
danielbloy <daniel_bloy@hotmail.com>
2024-10-24 18:41:50 +0100

    Added animations.

--- tests/on_device/0_validate_everything.py
+++ tests/on_device/0_validate_everything.py
@@ -17,18 +17,28 @@
 import time
 
 from buzzer import BuzzerController
+from interactive.animation import Flicker
 from interactive.button import ButtonController
 from interactive.directory import DirectoryService
 from interactive.environment import are_pins_available, is_running_on_microcontroller
-from interactive.log import set_log_level, info, INFO
+from interactive.led import Led
+from interactive.log import info
+from interactive.log import set_log_level, INFO
+from interactive.melody import MelodySequence, Melody, decode_melody
 from interactive.memory import report_memory_usage_and_free
 from interactive.network import NetworkController
+from interactive.polyfills.animation import AMBER, BLACK, WHITE, JADE, PINK, OLD_LACE, AnimationSequence
+from interactive.polyfills.animation import AQUA, RED, GOLD, YELLOW, ORANGE, GREEN
+from interactive.polyfills.animation import BLUE, CYAN, PURPLE, MAGENTA, TEAL
+from interactive.polyfills.animation import ColorCycle, Sparkle, Rainbow, RainbowComet, RainbowChase
+from interactive.polyfills.animation import RainbowSparkle, Blink, Chase, Pulse, Comet
 from interactive.polyfills.button import new_button
+from interactive.polyfills.buzzer import new_buzzer
+from interactive.polyfills.led import new_led_pin
 from interactive.polyfills.led import onboard_led
 from interactive.polyfills.network import new_server
+from interactive.polyfills.pixel import new_pixels
 from interactive.runner import Runner
-from melody import MelodySequence, Melody, decode_melody
-from polyfills.buzzer import new_buzzer
 
 REPORT_RAM = is_running_on_microcontroller()
 
@@ -177,7 +187,89 @@ steps.append({"name": "Runner with button and buzzer", "func": runner_with_butto
 # STEP 3: Runner with LEDs, NeoPixels and animations.
 # ********************************************************************************
 def runner_with_leds_pixels_animations() -> None:
-    pass
+    if REPORT_RAM:
+        report_memory_usage_and_free("Before executing runner_with_leds_pixels_animations")
+
+    runner = Runner()
+
+    yellow = Led(new_led_pin(LED_YELLOW))
+    green = Led(new_led_pin(LED_GREEN))
+    red = Led(new_led_pin(LED_RED))
+
+    red_animation = Blink(red, speed=0.5, color=WHITE)
+    green_animation = Pulse(green, speed=0.1, color=WHITE, period=2)
+
+    yellow_animations = [
+        Flicker(yellow, speed=0.1, color=WHITE),
+        Pulse(yellow, speed=0.1, color=WHITE, period=1),
+        Blink(yellow, speed=0.5, color=WHITE),
+    ]
+    yellow_animation = AnimationSequence(*yellow_animations, advance_interval=3)
+
+    async def animate_leds() -> None:
+        if not runner.cancel:
+            if red_animation:
+                red_animation.animate()
+            if green_animation:
+                green_animation.animate()
+            if yellow_animation:
+                yellow_animation.animate()
+
+    runner.add_loop_task(animate_leds)
+
+    pixels = new_pixels(PIXELS_PIN, 8, brightness=0.5)
+    animations = [
+        Flicker(pixels, speed=0.1, color=AMBER, spacing=2),
+        Blink(pixels, speed=0.5, color=JADE),
+        Comet(pixels, speed=0.01, color=PINK, tail_length=7, bounce=True),
+        Chase(pixels, speed=0.1, size=3, spacing=6, color=OLD_LACE),
+        ColorCycle(pixels, 0.5, colors=[RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, BLACK]),
+        Pulse(pixels, speed=0.1, color=AQUA, period=3),
+        Sparkle(pixels, speed=0.05, color=GOLD, num_sparkles=3),
+        Rainbow(pixels, speed=0.1, period=2),
+        RainbowComet(pixels, speed=0.1, tail_length=7, bounce=True),
+        RainbowChase(pixels, speed=0.1, size=5),
+        RainbowSparkle(pixels, speed=0.1, num_sparkles=3),
+    ]
+    animation = AnimationSequence(*animations, advance_interval=5)
+
+    async def animate_pixels() -> None:
+        if not runner.cancel:
+            if animation:
+                animation.animate()
+
+    runner.add_loop_task(animate_pixels)
+
+    # Allow the application to only run for a defined number of seconds.
+    finish = time.monotonic() + 5
+
+    async def callback() -> None:
+        runner.cancel = time.monotonic() > finish
+        if runner.cancel:
+            yellow_animation.freeze()
+            green_animation.freeze()
+            red_animation.freeze()
+
+            yellow.off()
+            green.off()
+            red.off()
+            yellow.show()
+            green.show()
+            red.show()
+
+            animation.freeze()
+            pixels.fill(BLACK)
+            pixels.write()
+
+    if REPORT_RAM:
+        report_memory_usage_and_free("Before running Runner")
+
+    runner.run(callback)
+
+    del animation, animations, pixels, yellow_animations, yellow_animation, green_animation, red_animation, reg, green, yello, runner
+
+    if REPORT_RAM:
+        report_memory_usage_and_free("After executing runner_with_leds_pixels_animations")
 
 
 steps.append({"name": "Runner with LEDs, pixels and animations", "func": runner_with_leds_pixels_animations})
