a54c15e5fedc392426dedaa389921455e5b03f92
danielbloy <daniel_bloy@hotmail.com>
2024-12-19 21:15:54 +0000

    Additional work.

--- interactive/microphone.py
+++ interactive/microphone.py
@@ -1,17 +1,52 @@
 from interactive.environment import is_running_on_desktop
-from interactive.polyfills.button import Button
+from interactive.polyfills.microphone import Microphone
 from interactive.runner import Runner
 
 # collections.abc is not available in CircuitPython.
 if is_running_on_desktop():
-    from collections.abc import Callable, Awaitable
+    pass
 
 
 class MicrophoneController:
+    """
+    TODO: Have a callback when each quantised value is returned
+    # TODO: Should this create a subtask when playing or use the update method.
 
-    def __init__(self, microphone: Microphone):
+    Instances of this class will need to register() with a Runner in order to work.
+    """
+
+    def __init__(self, microphone: Microphone, sample_rate=44100):
         if microphone is None:
             raise ValueError("microphone cannot be None")
 
         if not isinstance(microphone, Microphone):
             raise ValueError("microphone must be of type Microphone")
+
+        self.__runner = None
+        self.__microphone = microphone
+        self.recording = False
+        self.sample_rate = sample_rate
+
+    # TODO: Add recording handler
+
+    def register(self, runner: Runner) -> None:
+        """
+        Registers this MicrophoneController instance as a task with the provided Runner.
+
+        :param runner: the runner to register with.
+        """
+        self.__runner = runner
+        runner.add_loop_task(self.__loop)
+
+    async def __loop(self):
+        """
+        The internal loop simply invokes the correct handler based on the microphone state.
+        """
+        if self.__runner.cancel:
+            return
+
+        if not self.recording:
+            return
+
+        # TODO: need to convert the value which is clamped from min to max into an
+        #       ordinal value from zero to 100% (also include the raw value).
--- interactive/polyfills/microphone.py
+++ interactive/polyfills/microphone.py
@@ -1,52 +1,74 @@
 from interactive.environment import are_pins_available
 
-
-class Microphone:
-    """
-
-    TODO: Have a callback when each quantised value is returned
-    TODO: Record()
-    TODO: recording
-    TODO: Pause()
-    TODO: Paused
-    TODO: Resume()
-    TODO: Stop()
-    # TODO: Should this create a subtask when playing or use the update method.
-
-    Stub implementation of Microphone for Desktop without pins. Does nothing.
-    """
-
-    def __init__(self, pin, sample_rate: int):
-        pass
-
-
 if are_pins_available():
 
+    from analogio import AnalogIn
     from microcontroller import Pin
 
 
-    def __new_microphone(pin: Pin, sample_rate) -> Microphone:
+    class Microphone:
+        """
+        Implementation of Microphone with pins. This requires an analogue
+        input pin and returns the sample value that is clamped within the
+        provided range.
+        """
+
+        def __init__(self, pin, minimum: int = 0, maximum: int = 65535):
+            self.mic = AnalogIn(pin)
+            self.min = minimum
+            self.max = maximum
+
+        @property
+        def value(self) -> int:
+            """
+            Returns the analogue value within the given range.
+            """
+
+            # Keep the value within the minimum and maximum range.
+            value = self.mic.value
+            if value < self.min:
+                value = self.min
+            if value > self.max:
+                value = self.max
+
+            return value
+
+
+    def __new_microphone(pin: Pin) -> Microphone:
         """
         Sets up a basic microphone that receives a simple analogue value from an input pin.
         This is typical of a MAX4466 type device.
 
         :param pin: The pin to use for the microphone signal.
         """
-        return Microphone(pin, sample_rate)
+        return Microphone(pin)
 
 else:
+    class Microphone:
+        """
+        Stub implementation of Microphone for Desktop without pins. Does nothing.
+        """
 
-    def __new_microphone(pin, sample_rate) -> Button:
-        return Microphone(pin, sample_rate)
+        def __init__(self, pin, minimum: int = 0, maximum: int = 65535):
+            self.pin = pin
+            self.min = minimum
+            self.max = maximum
+
+        @property
+        def value(self) -> int:
+            return 0
 
 
-def new_microphone(pin, sample_rate) -> Microphone:
+    def __new_microphone(pin) -> Microphone:
+        return Microphone(pin)
+
+
+def new_microphone(pin) -> Microphone:
     """
     Returns a new Microphone that performs the microphone processing based on
     whether the code is running in CircuitPython where a pin will be provided
     or in a desktop environment where nothing will be done.
 
     :param pin: The pin to use for the microphone signal.
-    :param sample_rate: The number of samples per second.
     """
-    return __new_microphone(pin, sample_rate)
+    return __new_microphone(pin)
